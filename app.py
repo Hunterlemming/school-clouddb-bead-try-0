a804a7d7881baaa5f5f2e25373f3358a3ec99ff8
Hunterlemming <kklemming@gmail.com>
2021-05-15 10:06:22 +0200

    first successful db query

--- app.py
+++ app.py
@@ -12,6 +12,16 @@ def hello_world():
     return 'Hello World!'
 
 
+def db_demo():
+    cursor.execute("""select productid, productname from products order by productname;""")
+    product_rows = cursor.fetchall()
+    product_list = []
+    for row in product_rows:
+        product_list.append({'productid': row[0], 'productname': row[1]})
+    for product in product_list:
+        print(product)
+
+
 if __name__ == '__main__':
     try:
         connection = psycopg2.connect(user=db_login['user'], password=db_login['password'],
@@ -20,6 +30,7 @@ if __name__ == '__main__':
         cursor = connection.cursor()
         logging.info("app indul...")
         if connection:
-            app.run(debug=True)
+            db_demo()
+            # app.run(debug=True)
     except (Exception, Error) as error:
         print("Connect Error: ", error)
