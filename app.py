d613b346be7337940c7f700ad791d86d9c9d57f7
Hunterlemming <kklemming@gmail.com>
2021-05-16 15:40:08 +0200

    separation of concerns p2

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from database import get_database_tools
+from database import get_database_tools, list_orders
 import logging
 import psycopg2
 from psycopg2 import Error
@@ -51,29 +51,6 @@ def order_proc():
             return render_template('order_list.html', order_records=list_orders())
     except (Exception, Error) as ex:
         logging.error("Query or Order error: ", ex)
-
-
-def list_orders():
-    try:
-        cursor.execute("select * from last_orders;")
-        order_rows = cursor.fetchall()
-        order_list = []
-        for row in order_rows:
-            order_list.append(
-                {
-                    'date': row[0],
-                    'companyname': row[1],
-                    'country': row[2],
-                    'balance': row[3],
-                    'productname': row[4],
-                    'quantity': row[5],
-                    'value': row[6],
-                    'unitsinstock': row[7]
-                 }
-            )
-        return order_list
-    except (Exception, Error) as ex:
-        logging.error("Connection Error: ", ex)
 
 
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -6,16 +6,46 @@ from private.data import db_login
 logging.basicConfig(level=logging.INFO)
 
 
+_cursor = None
+
+
 def get_database_tools():
+    global _cursor
+
     try:
         connection = psycopg2.connect(user=db_login['user'], password=db_login['password'],
                                       host=db_login['host'], port=db_login['port'],
                                       database=db_login['database'])
-        cursor = connection.cursor()
+        _cursor = connection.cursor()
         return {
             "connection": connection,
-            "cursor": cursor
+            "cursor": _cursor
         }
     except (Exception, Error) as ex:
         logging.error("Connection Error: ", ex)
         return None
+
+
+def list_orders():
+    global _cursor
+
+    try:
+        _cursor.execute("select * from last_orders;")
+        order_rows = _cursor.fetchall()
+        order_list = []
+        for row in order_rows:
+            order_list.append(
+                {
+                    'date': row[0],
+                    'companyname': row[1],
+                    'country': row[2],
+                    'balance': row[3],
+                    'productname': row[4],
+                    'quantity': row[5],
+                    'value': row[6],
+                    'unitsinstock': row[7]
+                 }
+            )
+        return order_list
+    except (Exception, Error) as ex:
+        logging.error("Connection Error: ", ex)
