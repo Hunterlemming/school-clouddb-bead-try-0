1b44a1413810cdaeeb50be3e17cfc6580b8a77b6
Hunterlemming <kklemming@gmail.com>
2021-05-16 15:10:16 +0200

    basic gcp seems to work

--- app.py
+++ app.py
@@ -1,28 +1,81 @@
-import psycopg2
 import logging
+from flask import Flask, render_template, request
+
+import psycopg2
 from psycopg2 import Error
-from flask import Flask
 from private.data import db_login
 
 app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/order_form', methods=['POST', 'GET'])
+def order_form():
+    try:
+        cursor.execute("select productid, productname from products order by productname;")
+        product_rows = cursor.fetchall()
+        product_list = []
+        for row in product_rows:
+            product_list.append(
+                {
+                    'productid': row[0],
+                    'productname': row[1]
+                }
+            )
+        # customer combo feltöltése
+        cursor.execute("select customerid, companyname from customers order by companyname;")
+        cust_rows = cursor.fetchall()
+        cust_list = []
+        for row in cust_rows:
+            cust_list.append(
+                {
+                    'customerid': row[0],
+                    'companyname': row[1]
+                }
+            )
+        return render_template('order_form.html', product_records=product_list, cust_records=cust_list)
+    except (Exception, Error) as ex:
+        logging.error("Query error: ", ex)
 
 
-def db_demo():
-    cursor.execute("""select productid, productname from products order by productname;""")
-    product_rows = cursor.fetchall()
-    product_list = []
-    for row in product_rows:
-        product_list.append({'productid': row[0], 'productname': row[1]})
-    for product in product_list:
-        print(product)
+@app.route('/order_proc', methods=['POST', 'GET'])
+def order_proc():
+    try:
+        if request.method == 'POST':
+            sql = "select new_order(%s, %s, %s);"
+            with connection:
+                with connection.cursor() as c:
+                    c.execute(sql, (request.form['product'], request.form['qt'], request.form['customer'],))
+                    row = c.fetchone()
+                    return render_template('order_list.html', success=row[0], order_records=list_orders())
+        else:
+            return render_template('order_list.html', order_records=list_orders())
+    except (Exception, Error) as ex:
+        logging.error("Query or Order error: ", ex)
 
 
-# git token push
+def list_orders():
+    try:
+        cursor.execute("select * from last_orders;")
+        order_rows = cursor.fetchall()
+        order_list = []
+        for row in order_rows:
+            order_list.append(
+                {
+                    'date': row[0],
+                    'companyname': row[1],
+                    'country': row[2],
+                    'balance': row[3],
+                    'productname': row[4],
+                    'quantity': row[5],
+                    'value': row[6],
+                    'unitsinstock': row[7]
+                 }
+            )
+        return order_list
+    except (Exception, Error) as ex:
+        logging.error("Connection Error: ", ex)
 
 
 if __name__ == '__main__':
@@ -31,9 +84,8 @@ if __name__ == '__main__':
                                       host=db_login['host'], port=db_login['port'],
                                       database=db_login['database'])
         cursor = connection.cursor()
-        logging.info("app indul...")
         if connection:
-            db_demo()
-            # app.run(debug=True)
+            logging.info("app indul...")
+            app.run(debug=True)
     except (Exception, Error) as error:
-        print("Connect Error: ", error)
+        print("Connection Error: ", error)
---
+++ database.py
@@ -0,0 +1,6 @@
+import psycopg2
+from psycopg2 import Error
+from private.data import db_login
+
+
+def connect_to_database():
